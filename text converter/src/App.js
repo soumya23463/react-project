77520ed2499bad99a56b258388be858c678d1096
Soumya kesarwani <68148283+soumya23463@users.noreply.github.com>
2025-08-31 11:26:29 +0530

    Reapply "text converter app"
    This reverts commit 2553054801bfb1747d8f9162c9957054457e554d.

---
+++ text converter/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+
+function App() {
+  return (
+    <>
+   
+    <Navbar title="TextUtils"/>
+    <div className="container my-3">
+      <TextForm heading="Enter the text to analize"/>
+    </div>
+    
+    </>
+   
+  );
+}
+
+export default App;
---
+++ text converter/src/components/readme.txt
@@ -0,0 +1,2 @@
+npm install react-toastify
+        to add alert in react
